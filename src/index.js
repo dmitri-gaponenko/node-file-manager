b2f9faf3d4a756e25175a96387b9957a220f0c4d
Dmitri Gaponenko <dm.gaponenko@gmail.com>
2022-12-08 00:23:38 +0300

    feat: add welcome and goodbye messages and display start working directory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-console.log(`Welcome to the File Manager, Username!`);
+import readline from 'readline';
+import { stdin, stdout } from 'process';
+import { EOL, homedir } from 'os';
+
+let workingDirectory = homedir();
+const rl = readline.createInterface({ input: stdin, output: stdout });
+
+const getUserNameFromArgs = () => {
+  const prefix = '--username=';
+  const userNameArg = process.argv
+    .slice(2)
+    .find((arg) => arg.startsWith(prefix));
+
+  let userName = '';
+
+  try {
+    userName = userNameArg.slice(prefix.length);
+  } catch (err) {
+    console.log(`ERROR: '--username' parameter is not defined.`);
+  }
+
+  return userName;
+};
+
+const finish = () => {
+  process.stdout.write(`Thank you for using File Manager, ${getUserNameFromArgs()}, goodbye!${EOL}`);
+  rl.close();
+  process.exit();
+};
+
+const handle = (text) => {
+  //process.stdout.write(`${text.toUpperCase()}${EOL}`);
+
+  printWorkingDirectoryMessage();
+};
+
+rl.on('line', (text) => {
+  if (text === '.exit') {
+    finish();
+  }
+  handle(text);
+});
+
+rl.on('SIGINT', () => {
+  finish();
+});
+
+const printWorkingDirectoryMessage = () => {
+  console.log(`You are currently in ${workingDirectory}`);
+};
+
+const runApp = () => {
+  console.log(`Welcome to the File Manager, ${getUserNameFromArgs()}!${EOL}`);
+  printWorkingDirectoryMessage();
+};
+
+runApp();
