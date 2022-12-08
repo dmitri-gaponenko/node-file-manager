035806216c4cd3042c58d127cbc13a61828156c5
Dmitri Gaponenko <dm.gaponenko@gmail.com>
2022-12-08 13:20:53 +0300

    feat: add ls, up and cd commands

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readline from 'readline';
+import path from 'path';
 import { stdin, stdout } from 'process';
 import { EOL, homedir } from 'os';
+import fs from 'fs/promises';
 
 let workingDirectory = homedir();
 const rl = readline.createInterface({ input: stdin, output: stdout });
@@ -28,8 +30,41 @@ const finish = () => {
   process.exit();
 };
 
-const handle = (text) => {
-  //process.stdout.write(`${text.toUpperCase()}${EOL}`);
+// TODO: add sorting
+const list = async (filesFolderPath) => {
+  try {
+    const filesFolder = await fs.readdir(filesFolderPath, {
+      withFileTypes: true,
+    });
+    console.table(
+      filesFolder.map((file) => {
+        return { Name: file.name, Type: file.isFile() ? 'file' : 'directory' };
+      })
+    );
+  } catch (err) {
+    // throw new Error('FS operation failed');
+    return null;
+  }
+};
+
+const handle = async (text) => {
+  const [command, ...arg] = text.split(' ');
+
+  switch (command) {
+    case 'ls':
+      await list(workingDirectory);
+      break;
+    case 'up':
+      workingDirectory = path.join(workingDirectory, '../');
+      break;
+    case 'cd':
+      workingDirectory = path.join(workingDirectory, arg.join(' '));
+      // TODO: check folder/file is exist
+      break;  
+
+    default:
+      break;
+  }
 
   printWorkingDirectoryMessage();
 };
